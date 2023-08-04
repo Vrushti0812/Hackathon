98f85d5db5eea2c204fe1fd51398f85b9e16eba3
Ixhika <106822722+Ixhika@users.noreply.github.com>
2023-08-04 16:38:03 +0530

    added toast

--- app/src/main/java/com/vrushti/hackathon/MainActivity.java
+++ app/src/main/java/com/vrushti/hackathon/MainActivity.java
@@ -3,12 +3,23 @@ package com.vrushti.hackathon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button but;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        but = findViewById(R.id.button);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "hello", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
