43af7c77c519e94508734f87bf73cf09225ae2ab
Vrushti0812 <132557222+Vrushti0812@users.noreply.github.com>
2023-08-05 18:26:17 +0530

    new

--- app/src/main/java/com/vrushti/hackathon/MainActivity2.java
+++ app/src/main/java/com/vrushti/hackathon/MainActivity2.java
@@ -3,6 +3,7 @@ package com.vrushti.hackathon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Window window = this.getWindow();
+        window.setStatusBarColor(this.getResources().getColor(R.color.green3));
     }
 }
