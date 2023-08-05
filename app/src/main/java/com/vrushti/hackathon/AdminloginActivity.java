9978bfef1662d98ca9758560df1a58092403d716
Ixhika <106822722+Ixhika@users.noreply.github.com>
2023-08-05 14:35:31 +0530

    firebase

--- app/src/main/java/com/vrushti/hackathon/AdminloginActivity.java
+++ app/src/main/java/com/vrushti/hackathon/AdminloginActivity.java
@@ -3,12 +3,30 @@ package com.vrushti.hackathon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AdminloginActivity extends AppCompatActivity {
-
+    EditText aname , apassword;
+    Button lgin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.adminlogin);
+        Window window = this.getWindow();
+        window.setStatusBarColor(this.getResources().getColor(R.color.green3));
+
+        aname = findViewById(R.id.username);
+        apassword = findViewById(R.id.password);
+        lgin = findViewById(R.id.button);
+
+        lgin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+    
+            }
+        });
     }
 }
--- app/src/main/java/com/vrushti/hackathon/MainActivity.java
+++ app/src/main/java/com/vrushti/hackathon/MainActivity.java
@@ -2,6 +2,7 @@ package com.vrushti.hackathon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -18,6 +19,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Window window = this.getWindow();
         window.setStatusBarColor(this.getResources().getColor(R.color.green3));
+        admin = findViewById(R.id.buttonAdmin);
+        student = findViewById(R.id.buttonStudent);
 
+        admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this , AdminloginActivity.class);
+                startActivity(intent);
+            }
+        });
+//        student.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this , StudentLogin.class);
+//                startActivity(intent);
+//            }
+//        });
     }
 }
