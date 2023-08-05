5da0975f7038a61c90c33bd19d6cea1cc9b994e1
Ixhika <106822722+Ixhika@users.noreply.github.com>
2023-08-06 05:13:25 +0530

    firebasenew

--- app/src/main/java/com/vrushti/hackathon/Complaints.java
+++ app/src/main/java/com/vrushti/hackathon/Complaints.java
@@ -1,20 +1,52 @@
 package com.vrushti.hackathon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class Complaints extends AppCompatActivity {
 
-
+ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complaints);
+        listView = findViewById(R.id.listView);
 
+        ArrayList<String> list = new ArrayList<>();
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.list_item,list);
+        listView.setAdapter(adapter);
+
+        DatabaseReference reference  = FirebaseDatabase.getInstance().getReference().child("Complaints");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list.clear();
+                for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    list.add(dataSnapshot.getValue().toString());
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/vrushti/hackathon/Hostel.java
+++ app/src/main/java/com/vrushti/hackathon/Hostel.java
@@ -20,7 +20,7 @@ public class Hostel extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hostel);
-      
+
 
 
 
