797212801091ef0ef3d1d4b35a7015f37a55f60c
Vrushti0812 <132557222+Vrushti0812@users.noreply.github.com>
2023-08-05 14:07:21 +0530

    added new

---
+++ app/src/main/java/com/vrushti/hackathon/AdminloginActivity.java
@@ -0,0 +1,14 @@
+package com.vrushti.hackathon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminloginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.adminlogin);
+    }
+}
