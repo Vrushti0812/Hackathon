2a187fa10d95451d92dd02f0c25d219f7e080bba
Vrushti0812 <132557222+Vrushti0812@users.noreply.github.com>
2023-08-06 02:53:07 +0530

    new

--- app/src/main/java/com/vrushti/hackathon/HMSStudentPage1.java
+++ app/src/main/java/com/vrushti/hackathon/HMSStudentPage1.java
@@ -1,17 +1,23 @@
 package com.vrushti.hackathon;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.internal.DescendantOffsetUtils;
 
+import java.util.Random;
+
 public class HMSStudentPage1 extends AppCompatActivity {
-    Button mess , outing , complaints , night , parlour , cleaning;
+    Button mess, outing, complaints, night, parlour, cleaning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,44 +36,125 @@ public class HMSStudentPage1 extends AppCompatActivity {
         mess.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HMSStudentPage1.this , Mess.class);
+                Intent intent = new Intent(HMSStudentPage1.this, Mess.class);
                 startActivity(intent);
             }
         });
         outing.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HMSStudentPage1.this , Outing.class);
+                Intent intent = new Intent(HMSStudentPage1.this, Outing.class);
                 startActivity(intent);
             }
         });
         complaints.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HMSStudentPage1.this , StudentComplaint.class);
-                startActivity(intent);
+                showComplaintDialog();
             }
         });
         night.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HMSStudentPage1.this , Canteen.class);
+                Intent intent = new Intent(HMSStudentPage1.this, Canteen.class);
                 startActivity(intent);
             }
         });
         parlour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HMSStudentPage1.this , Parlour.class);
-                startActivity(intent);
+                showRandomAppointmentTime();
             }
         });
         cleaning.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(HMSStudentPage1.this , Cleaning.class);
-                startActivity(intent);
+                showConfirmationDialog();
             }
         });
+    }
+
+    private void showConfirmationDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Cleaning Request");
+        builder.setMessage("Your room cleaning has been requested.");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Handle OK button click if needed, or leave it empty
+                dialog.dismiss(); // Dismiss the dialog
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void showRandomAppointmentTime() {
+        // Generate a random appointment time between 9:00 AM and 6:00 PM
+        Random random = new Random();
+        int hour = random.nextInt(9) + 9; // Generate random hour (9 to 17)
+        int minute = random.nextInt(60);  // Generate random minute (0 to 59)
+
+        // Format the time as a string
+        String time = String.format("%02d:%02d", hour, minute);
+
+        // Create the AlertDialog with the random appointment time and "Get Other Time" button
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Parlor Appointment");
+        builder.setMessage("Your appointment is at: " + time);
+        builder.setPositiveButton("Get Other Time", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Generate another random appointment time when "Get Other Time" is clicked
+                showRandomAppointmentTime();
+            }
+        });
+        builder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Handle OK button click if needed, or leave it empty
+                dialog.dismiss(); // Dismiss the dialog
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+
+    private void showComplaintDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Submit Complaint");
+
+        // Inflate the complaint_dialog.xml layout
+        View view = getLayoutInflater().inflate(R.layout.complaint_dialog, null);
+        builder.setView(view);
+
+         EditText complaintEditText = view.findViewById(R.id.et);
+
+        builder.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Get the user's complaint text from the EditText
+                String complaintText = complaintEditText.getText().toString().trim();
+
+                // Process the complaint, you can send it to your server or store it in a database
+                // For this example, we'll just show a Toast message
+                if (!complaintText.isEmpty()) {
+                    Toast.makeText(HMSStudentPage1.this, "Complaint submitted: " + complaintText, Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(HMSStudentPage1.this, "Please enter your complaint.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss(); // Dismiss the dialog when "Cancel" is clicked
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
--- app/src/main/java/com/vrushti/hackathon/Outing.java
+++ app/src/main/java/com/vrushti/hackathon/Outing.java
@@ -4,20 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import android.os.Bundle;
 import android.view.Window;
+
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.Writer;
+import com.google.zxing.WriterException;
 
 public class Outing extends AppCompatActivity {
 
     EditText name, regno, phnno;
     Button btn;
     Spinner sp1, sp2;
+    ImageView imageView;
 
 
     @Override
@@ -28,27 +41,72 @@ public class Outing extends AppCompatActivity {
         window.setStatusBarColor(this.getResources().getColor(R.color.green3));
 
 
-        name=findViewById(R.id.name);
-        regno = findViewById(R.id.regnumber);
+        name = findViewById(R.id.name);
+        regno = findViewById(R.id.regno);
         phnno = findViewById(R.id.phnno);
-        btn= findViewById(R.id.gbtn);
-        sp1=findViewById(R.id.spinner1);
-        sp2=findViewById(R.id.spinner2);
+        btn = findViewById(R.id.gbtn);
+        sp1 = findViewById(R.id.spinner1);
+        sp2 = findViewById(R.id.spinner2);
+        imageView = findViewById(R.id.qr);
 
-        String[] G = {"LH-1" , "LH-2", "LH-3"};
-       ArrayAdapter ad = new ArrayAdapter<String>(Outing.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,G);
+        String[] G = {"LH-1", "LH-2", "LH-3"};
+        ArrayAdapter ad = new ArrayAdapter<String>(Outing.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, G);
         sp1.setAdapter(ad);
 
-        String[] S = {"9:00-5:00" , "10:00-6:00", "11:00-7:00"};
-        ArrayAdapter ad1 = new ArrayAdapter<String>(Outing.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,S);
-        sp1.setAdapter(ad1);
+        String[] S = {"9:00-5:00", "10:00-6:00", "11:00-7:00"};
+        ArrayAdapter ad1 = new ArrayAdapter<String>(Outing.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, S);
+        sp2.setAdapter(ad1);
 
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = name.getText().toString();
+                String registrationNumber = regno.getText().toString();
+                String phoneNumber = phnno.getText().toString();
+                String selectedBuilding = sp1.getSelectedItem().toString();
+                String selectedTimeSlot = sp2.getSelectedItem().toString();
 
+                // Concatenate all the information into a single string
+                String userInfo = "Name: " + userName + "\n" +
+                        "Registration Number: " + registrationNumber + "\n" +
+                        "Phone Number: " + phoneNumber + "\n" +
+                        "Selected Building: " + selectedBuilding + "\n" +
+                        "Selected Time Slot: " + selectedTimeSlot;
 
+                generateQRCode(userInfo);
+            }
+        });
     }
 
+    private void generateQRCode(String data) {
+        // Generate QR code
+        QRCodeWriter writer = new QRCodeWriter();
+        try {
 
+            BitMatrix bitMatrix = writer.encode(data, BarcodeFormat.QR_CODE, 512, 512);
+            int width = bitMatrix.getWidth();
+            int height = bitMatrix.getHeight();
+            Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    bitmap.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+                }
+            }
+            imageView.setImageBitmap(bitmap); // Set the generated QR code to the ImageView
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
     }
+}
+
+
+
+
+
+
+
+
+
 
 
 
